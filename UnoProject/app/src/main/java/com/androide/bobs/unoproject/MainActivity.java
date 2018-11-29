26ed0ec6e5765a1749e012014280cc13f56535b2
Mateus Podgorski <mateus-pk@outlook.com>
2018-11-28 22:11:45 -0200

    Start Screen Start
    Start screen animation start.

--- UnoProject/app/src/main/java/com/androide/bobs/unoproject/MainActivity.java
+++ UnoProject/app/src/main/java/com/androide/bobs/unoproject/MainActivity.java
@@ -18,9 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnPlay;
     private Button btnQuit;
 
-    ArrayList<Integer> songs = new ArrayList<>();
 
-    MediaPlayer soundtrack = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +26,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnPlay = findViewById(R.id.btnPlay);
         btnQuit = findViewById(R.id.btnQuit);
-
-        songs.add(R.raw.megaman);
-
-        // criar metodo random que escolhe a musica de fundo
-
-        soundtrack.stop();
-        soundtrack = MediaPlayer.create(MainActivity.this, songs.get(0));
-        soundtrack.start();
-        soundtrack.setLooping(true);
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
